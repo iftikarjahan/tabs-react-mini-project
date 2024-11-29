ff5014936803346e6ad435861ab6a69d91173860
iftikarjahan <iftikarjahan22@gmail.com>
2024-11-29 19:30:05 +0530

    added comment

--- src/components/JobDescriptionContainer.jsx
+++ src/components/JobDescriptionContainer.jsx
@@ -3,6 +3,10 @@ import Duties from "./Duties";
 const JobDescriptionContainer = ({ jobs, currentSelectedJob }) => {
   // console.log(jobs);
 
+/*
+->Based on the currentSelectedJob state-that we are creating on the CompanyListBtnContainer,
+we are displaying this component
+*/   
   const { company, dates, duties, title } = jobs[currentSelectedJob];
 
   return (
